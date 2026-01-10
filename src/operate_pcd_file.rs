ad24dfc72b9c525602b125a04a93c3b768d5cf7c
Kenji0510 <yukkurireimupikacyuu@gmail.com>
2026-01-11 02:58:00 +0900

    20260111

--- src/operate_pcd_file.rs
+++ src/operate_pcd_file.rs
@@ -80,3 +80,25 @@ pub fn load_pcd_xyzt(
 
     Ok(points)
 }
+
+pub fn load_pcd_xyz(
+    file_path: &str,
+) -> Result<Vec<PointXYZ>> {
+    let reader = match Reader::open(file_path) {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("Failed to open PCD file: {}", e);
+            return Err(anyhow::anyhow!("Failed to open PCD file: {}", e));
+        }
+    };
+
+    let points: Vec<PointXYZ> = match reader.collect() {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("Failed to read PCD data: {}", e);
+            return Err(anyhow::anyhow!("Failed to read PCD data: {}", e));
+        }
+    };
+
+    Ok(points)
+}
